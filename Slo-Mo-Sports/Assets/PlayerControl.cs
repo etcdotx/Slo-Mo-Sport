f775413383dd3a5475b2ac2521f1d948b7265812
etcdotx <36808121+etcdotx@users.noreply.github.com>
2018-03-24 14:52:51 +0700

    sada
    fdsfdssdas

---
+++ Slo-Mo-Sports/Assets/PlayerControl.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour {
+    public float speed;
+    public float sensitivityX = 15F;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.W)) {
+            transform.Translate(0, 0, speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(-speed * Time.deltaTime, 0,0);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(speed * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(0, 0,-speed * Time.deltaTime);
+        }
+        float rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;
+        transform.localEulerAngles = new Vector3(0, rotationX, 0);
+    }
+}
