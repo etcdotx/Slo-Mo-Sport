3a9381e42215301f5646ec330a99631d816dbe68
etcdotx <36808121+etcdotx@users.noreply.github.com>
2018-04-09 14:36:45 +0700

    s
     s

---
+++ Slo-Mo-Sports/Assets/Weapon.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour {
+
+    public float damage = 10f;
+    public float range = 100f;
+
+    public Camera fpscamera;
+	void Update () {
+        if (Input.GetMouseButtonDown(1)) {
+            shoot();
+        }
+	}
+
+    void shoot() {
+        RaycastHit hit;
+        if (Physics.Raycast(fpscamera.transform.position, fpscamera.transform.forward, out hit, range)) {
+            Debug.Log("HIT");
+        }
+    }
+	
+
+}
